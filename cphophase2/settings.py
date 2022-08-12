c44b89c906f20edf45846defb3fda923cc800af9
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-08-12 12:24:28 -0700

    Fix access from django server

--- cphophase2/settings.py
+++ cphophase2/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:8000']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

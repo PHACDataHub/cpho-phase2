54bbaf0abb951af56210d32259c88d34cfea163f
Eduardo Szeckir <eduardo.szeckir@phac-aspc.gc.ca>
2022-11-02 16:30:37 -0400

    Adding apollo client for graphql frontend

--- app/schema.py
+++ app/schema.py
@@ -8,6 +8,16 @@ class IndicatorType(DjangoObjectType):
         model = Indicator
 
 
+class PossibleIndicatorResponseType(graphene.ObjectType):
+    id = graphene.Int()
+    name = graphene.String()
+    category = graphene.String()
+    data_point_count = graphene.Int()
+
+    # def resolve_data_point_count(self, info, **kwargs):
+    #     return IndicatorData.objects.filter(indicator=self).count()
+
+
 class IndicatorDataType(DjangoObjectType):
     class Meta:
         model = IndicatorData
@@ -22,27 +32,28 @@ class TrendAnalysisType(DjangoObjectType):
     class Meta:
         model = TrendAnalysis
         fields = (
-            'id',
-            'indicator',
-            'detailed_indicator',
-            'year',
-            'data_point',
-            'line_of_best_fit_point',
+            "id",
+            "indicator",
+            "detailed_indicator",
+            "year",
+            "data_point",
+            "line_of_best_fit_point",
         )
 
 
-class CreateIndicator(graphene.Mutation):
-    class Arguments:
-        category = graphene.String(required=True)
-        topic = graphene.String(required=True)
-        indicator = graphene.String(required=True)
-        detailed_indicator = graphene.String(required=True)
-        sub_indicator_measurement = graphene.String()
+# class CreateIndicator(graphene.Mutation):
+#     class Arguments:
+#         category = graphene.String(required=True)
+#         topic = graphene.String(required=True)
+#         indicator = graphene.String(required=True)
+#         detailed_indicator = graphene.String(required=True)
+#         sub_indicator_measurement = graphene.String()
 
 
 class Query(graphene.ObjectType):
     indicators = graphene.List(IndicatorType)
     indicator_data = graphene.List(IndicatorDataType)
+    possible_indicators = graphene.List(PossibleIndicatorResponseType)
 
     def resolve_indicators(root, info, **kwargs):
         # Querying a list
@@ -51,6 +62,22 @@ class Query(graphene.ObjectType):
     def resolve_indicator_data(root, info, **kwargs):
         # Querying a list
         return IndicatorData.objects.all()
+
+    def resolve_possible_indicators(root, info, **kwargs):
+        # indicators = Indicator.objects.all().values()
+        response = []
+        for ind in Indicator.objects.all():
+            response.append(
+                PossibleIndicatorResponseType(
+                    id=ind.id,
+                    name=ind.indicator,
+                    category=ind.category,
+                    data_point_count=IndicatorData.objects.filter(
+                        indicator=ind.id
+                    ).count(),
+                )
+            )
+        return response
 
 
 schema = graphene.Schema(query=Query)

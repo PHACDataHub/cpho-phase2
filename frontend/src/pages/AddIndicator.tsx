68f6f700aab5913b11cb43cbc2524ba835df0f52
Eduardo Szeckir <eduardo.szeckir@phac-aspc.gc.ca>
2022-11-08 22:12:49 -0500

    latest

--- frontend/src/pages/AddIndicator.tsx
+++ frontend/src/pages/AddIndicator.tsx
@@ -1,191 +1,21 @@
-import { useMutation } from "@apollo/client";
-import {
-  VStack,
-  Heading,
-  Input,
-  Select,
-  Button,
-  Stack,
-  FormControl,
-  FormLabel,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { categories, sub_categories } from "../utils/constants";
-import { CREATE_INDICATOR } from "../utils/graphql/mutations";
-import {
-  GET_INDICATOR_OVERVIEW,
-  GET_INDICATORS_AND_IDS,
-} from "../utils/graphql/queries";
+import { Flex } from "@chakra-ui/react";
 import { useSmallScreen } from "../utils/hooks";
-import { DataPoint, SubCategory } from "../utils/types";
-import { AddDataPointButton } from "./components/AddDataPointButton";
-import { DataPointContainer } from "./components/DataPointContainer";
+import IndicatorForm from "./components/addIndicatorForm/IndicatorForm";
 import { Page } from "./Page";
 
 export function AddIndicator() {
-  const [filteredSubCategories, setFilteredSubCategories] =
-    useState<SubCategory[]>(sub_categories);
-
-  const [values, setValues] = useState({
-    indicatorName: "",
-    detailedIndicator: "",
-    category: 1,
-    subCategory: 1,
-    dataPoints: [] as DataPoint[],
-  });
-
-  const {
-    indicatorName,
-    detailedIndicator,
-    category,
-    subCategory,
-    dataPoints,
-  } = values;
-
-  const setField = (field: string, value: any) => {
-    setValues({
-      ...values,
-      [field]: value,
-    });
-  };
-
   const smallScreen = useSmallScreen();
-
-  useEffect(() => {
-    setFilteredSubCategories(
-      sub_categories.filter((c) => c.category === category)
-    );
-  }, [category]);
-
-  const generalInfo = (
-    <VStack spacing={5} w={smallScreen ? "90%" : "50%"}>
-      <FormControl isRequired>
-        <FormLabel fontSize="2xl" fontWeight="bold">
-          Indicator Name
-        </FormLabel>
-        <Input
-          value={indicatorName}
-          onChange={(e) => setField("indicatorName", e.target.value)}
-          required
-          variant="filled"
-          placeholder="Enter indicator name"
-        />
-      </FormControl>
-      <FormControl isRequired>
-        <FormLabel fontSize="2xl" fontWeight="bold">
-          Category
-        </FormLabel>
-        <Select
-          required
-          variant="filled"
-          value={category}
-          onChange={(e) => setField("category", parseInt(e.target.value))}
-        >
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.label}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl>
-        <FormLabel fontSize="2xl" fontWeight="bold">
-          Sub Category
-        </FormLabel>
-        <Select
-          variant="filled"
-          value={subCategory}
-          onChange={(e) => setField("subCategory", parseInt(e.target.value))}
-        >
-          {filteredSubCategories.map((s) => (
-            <option key={s.id} value={s.id}>
-              {s.label}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl>
-        <FormLabel fontSize="2xl" fontWeight="bold">
-          Detailed Indicator
-        </FormLabel>
-        <Input
-          value={detailedIndicator}
-          onChange={(e) => setField("detailedIndicator", e.target.value)}
-          variant="filled"
-          placeholder="Enter detailed indicator"
-        />
-      </FormControl>
-    </VStack>
-  );
-
-  const [createIndicator, { loading, error, data }] = useMutation(
-    CREATE_INDICATOR,
-    {
-      refetchQueries: [
-        {
-          query: GET_INDICATOR_OVERVIEW,
-        },
-        {
-          query: GET_INDICATORS_AND_IDS,
-        },
-      ],
-    }
-  );
 
   return (
     <Page backButton={{ show: true, redirectUrl: "/" }} title="Add Indicator">
-      <Stack
-        direction={smallScreen ? "column" : "row"}
+      <Flex
         w={smallScreen ? "95%" : "90%"}
         margin="auto"
-        my={10}
-        display="flex"
         justify="space-around"
         align="flex-start"
       >
-        {generalInfo}
-        <VStack w={smallScreen ? "90%" : "50%"}>
-          <AddDataPointButton
-            dataPoints={dataPoints}
-            setDataPoints={(d) => setField("dataPoints", d)}
-          />
-          <DataPointContainer
-            setDataPoints={(d) => setField("dataPoints", d)}
-            dataPoints={dataPoints}
-          />
-        </VStack>
-      </Stack>
-      <VStack>
-        {loading && <Heading size="md">Loading...</Heading>}
-        {error && <Heading size="md">Error: {error.message}</Heading>}
-        {data && <Heading size="md">Success!</Heading>}
-        <Button
-          w="40%"
-          disabled={dataPoints.length === 0}
-          colorScheme="green"
-          onClick={() => {
-            createIndicator({
-              variables: {
-                category:
-                  categories.find((c) => c.id === category)?.label ?? "",
-                detailedIndicator,
-                indicator: indicatorName,
-                subIndicatorMeasurement: "",
-                topic:
-                  sub_categories.find((c) => c.id === subCategory)?.label ?? "",
-                dataPoints: dataPoints.map((d) => ({
-                  ...d,
-                  value: +d.value, // Gets number representation for float values to fix GraphQL query error
-                  valueLowerBound: +d.valueLowerBound,
-                  valueUpperBound: +d.valueUpperBound,
-                })),
-              },
-            });
-          }}
-        >
-          Submit
-        </Button>
-      </VStack>
+        <IndicatorForm />
+      </Flex>
     </Page>
   );
 }
--- frontend/src/pages/components/AddDataPointModal.tsx
+++ frontend/src/pages/components/AddDataPointModal.tsx
@@ -19,11 +19,18 @@ import {
   Button,
   ModalFooter,
   Select,
+  HStack,
+  VStack,
+  Icon,
+  Heading,
 } from "@chakra-ui/react";
 import { FormEvent, useState } from "react";
 import { IndicatorDataFields } from "../../utils/constants";
 import { useSmallScreen } from "../../utils/hooks";
 import { DataPoint } from "../../utils/types";
+import { IoIosGlobe } from "react-icons/io";
+import { CgHashtag } from "react-icons/cg";
+import { AiOutlineCalendar } from "react-icons/ai";
 
 export function AddDataPointModal({
   dataPointIdx,
@@ -42,73 +49,89 @@ export function AddDataPointModal({
 
   const dataPoint = dataPoints[dataPointIdx!];
 
-  const [yearType, setYearType] = useState<"single" | "range">(
-    dataPoint ? (dataPoint.singleYearTimeframe ? "single" : "range") : "single"
-  );
-  const [year1, setYear1] = useState<number>(
-    dataPoint
+  const [fields, setFields] = useState<{
+    yearType: "SINGLE" | "RANGE";
+    year1: number;
+    year2: number;
+    geographyType: "COUNTRY" | "REGION" | "PROVINCE_TERRITORY";
+    location: string;
+  }>({
+    yearType: dataPoint
+      ? dataPoint.singleYearTimeframe
+        ? "SINGLE"
+        : "RANGE"
+      : "SINGLE",
+    year1: dataPoint
       ? (dataPoint.singleYearTimeframe as unknown as number) ?? 2022
-      : 2022
-  );
-  const [year2, setYear2] = useState<number>(2023);
+      : 2022,
+    year2: dataPoint
+      ? (dataPoint.singleYearTimeframe as unknown as number) ?? 2022
+      : 2022,
+    geographyType: dataPoint
+      ? (dataPoint.geography as "COUNTRY" | "REGION" | "PROVINCE_TERRITORY")
+      : "COUNTRY",
+    location: dataPoint ? dataPoint.country : "",
+  });
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  const { yearType, year1, year2, geographyType, location } = fields;
 
-    console.log(event);
-    const point: DataPoint = {
-      country:
-        (document.getElementById("dp_location") as HTMLInputElement)?.value ??
-        "",
-      geography:
-        (document.getElementById("dp_geography") as HTMLInputElement)?.value ??
-        "",
-      sex: (document.getElementById("dp_sex") as HTMLInputElement)?.value ?? "",
-      gender:
-        (document.getElementById("dp_gender") as HTMLInputElement)?.value ?? "",
-      ageGroup:
-        (document.getElementById("dp_age_group") as HTMLInputElement).value ??
-        "",
-      ageGroupType:
-        (document.getElementById("dp_age_group_type") as HTMLInputElement)
-          .value ?? "",
-      dataQuality:
-        (document.getElementById("dp_data_quality") as HTMLInputElement)
-          .value ?? "",
-      value:
-        ((document.getElementById("dp_value") as HTMLInputElement)
-          .value as unknown as number) ?? 0,
-      valueUnit:
-        (document.getElementById("dp_value_unit") as HTMLInputElement).value ??
-        "",
-      valueLowerBound:
-        ((document.getElementById("dp_value_lower_bound") as HTMLInputElement)
-          .value as unknown as number) ?? 0,
-      valueUpperBound:
-        ((document.getElementById("dp_value_upper_bound") as HTMLInputElement)
-          .value as unknown as number) ?? 0,
-      singleYearTimeframe: yearType === "single" ? `${year1}` : undefined,
-      multiYearTimeframe:
-        yearType === "range" ? `${year1}-${year2}` : undefined,
-    };
+  // const handleSubmit = (event: FormEvent) => {
+  // //   event.preventDefault();
 
-    if (setDataPoints) {
-      console.log(dataPoint);
-      if (dataPoint) {
-        console.log("Modifying existing data point at index ", dataPointIdx);
-        setDataPoints(
-          dataPoints
-            .slice(0, dataPointIdx!)
-            .concat([point])
-            .concat(dataPoints.slice(dataPointIdx! + 1))
-        );
-      } else {
-        setDataPoints([...dataPoints, point]);
-      }
-    }
+  // //   console.log(event);
+  // //   const point: DataPoint = {
+  // //     country:
+  // //       (document.getElementById("dp_location") as HTMLInputElement)?.value ??
+  // //       "",
+  // //     geography:
+  // //       (document.getElementById("dp_geography") as HTMLInputElement)?.value ??
+  // //       "",
+  // //     sex: (document.getElementById("dp_sex") as HTMLInputElement)?.value ?? "",
+  // //     gender:
+  // //       (document.getElementById("dp_gender") as HTMLInputElement)?.value ?? "",
+  // //     ageGroup:
+  // //       (document.getElementById("dp_age_group") as HTMLInputElement).value ??
+  // //       "",
+  // //     ageGroupType:
+  // //       (document.getElementById("dp_age_group_type") as HTMLInputElement)
+  // //         .value ?? "",
+  // //     dataQuality:
+  // //       (document.getElementById("dp_data_quality") as HTMLInputElement)
+  // //         .value ?? "",
+  // //     value:
+  // //       ((document.getElementById("dp_value") as HTMLInputElement)
+  // //         .value as unknown as number) ?? 0,
+  // //     valueUnit:
+  // //       (document.getElementById("dp_value_unit") as HTMLInputElement).value ??
+  // //       "",
+  // //     valueLowerBound:
+  // //       ((document.getElementById("dp_value_lower_bound") as HTMLInputElement)
+  // //         .value as unknown as number) ?? 0,
+  // //     valueUpperBound:
+  // //       ((document.getElementById("dp_value_upper_bound") as HTMLInputElement)
+  // //         .value as unknown as number) ?? 0,
+  // //     singleYearTimeframe: yearType === "single" ? `${year1}` : undefined,
+  // //     multiYearTimeframe:
+  // //       yearType === "range" ? `${year1}-${year2}` : undefined,
+  // //   };
 
-    onClose();
-  };
+  //   if (setDataPoints) {
+  //     console.log(dataPoint);
+  //     if (dataPoint) {
+  //       console.log("Modifying existing data point at index ", dataPointIdx);
+  //       setDataPoints(
+  //         dataPoints
+  //           .slice(0, dataPointIdx!)
+  //           .concat([point])
+  //           .concat(dataPoints.slice(dataPointIdx! + 1))
+  //       );
+  //     } else {
+  //       setDataPoints([...dataPoints, point]);
+  //     }
+  //   }
+
+  //   onClose();
+  // };
 
   return (
     <Modal size="2xl" isOpen={isOpen} onClose={onClose}>
@@ -116,110 +139,170 @@ export function AddDataPointModal({
       <ModalContent>
         <ModalHeader>Add Data Point</ModalHeader>
         <ModalCloseButton />
-
-        <form onSubmit={handleSubmit}>
-          <ModalBody>
-            <Box display="flex" flexWrap="wrap" alignContent="stretch">
-              {IndicatorDataFields.map((field) => {
-                // console.log("field: ", field);
-                // console.log(
-                //   "dataPoint: ",
-                //   dataPoint && dataPoint[field.dpField]
-                // );
-                return (
-                  <Box
-                    key={field.id}
-                    flexGrow={1}
-                    w={smallScreen ? "100%" : "45%"}
-                    m={3}
-                  >
-                    <FormControl isRequired={field.required}>
-                      <FormLabel htmlFor={field.id}>{field.name}</FormLabel>
-                      {field.type === "text" ? (
-                        <Input
-                          placeholder={field.placeholder ?? ""}
-                          id={field.id}
-                          defaultValue={dataPoint && dataPoint[field.dpField]}
-                        />
-                      ) : field.type === "number" ? (
-                        <NumberInput
-                          defaultValue={dataPoint && dataPoint[field.dpField]}
-                          id={field.id}
-                          precision={2}
-                          step={0.01}
-                        >
-                          <NumberInputField />
-                          <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                          </NumberInputStepper>
-                        </NumberInput>
-                      ) : (
-                        field.options && (
-                          <Select
-                            id={field.id}
-                            placeholder={`Select ${field.name}`}
-                            defaultValue={dataPoint && dataPoint[field.dpField]}
-                          >
-                            {field.options.map((option, idx) => (
-                              <option key={idx} value={option.value}>
-                                {option.label}
-                              </option>
-                            ))}
-                          </Select>
-                        )
-                      )}
-                    </FormControl>
-                  </Box>
-                );
-              })}
-              <Stack w="100%" m={3}>
+        <ModalBody>
+          <VStack w="100%" align="start" gap={5}>
+            <VStack align="start">
+              <HStack>
+                <Icon as={IoIosGlobe} fontSize="xl" />
+                <Heading size="md" display="flex" alignItems="center">
+                  Geography
+                </Heading>
+              </HStack>
+              <HStack>
                 <FormControl isRequired>
                   <ButtonGroup isAttached>
                     <Button
                       size="sm"
-                      onClick={() => setYearType("single")}
-                      isActive={yearType === "single"}
+                      isActive={geographyType === "COUNTRY"}
+                      onClick={() =>
+                        setFields({ ...fields, geographyType: "COUNTRY" })
+                      }
+                    >
+                      Canada
+                    </Button>
+                    <Button
+                      size="sm"
+                      isActive={geographyType === "PROVINCE_TERRITORY"}
+                      onClick={() =>
+                        setFields({
+                          ...fields,
+                          geographyType: "PROVINCE_TERRITORY",
+                        })
+                      }
+                    >
+                      Province & Territory
+                    </Button>
+                    <Button
+                      size="sm"
+                      isActive={geographyType === "REGION"}
+                      onClick={() =>
+                        setFields({ ...fields, geographyType: "REGION" })
+                      }
+                    >
+                      Region
+                    </Button>
+                  </ButtonGroup>
+                </FormControl>
+                {geographyType !== "COUNTRY" && (
+                  <FormControl isRequired>
+                    <Select
+                      size="sm"
+                      placeholder={`Select ${
+                        geographyType === "PROVINCE_TERRITORY"
+                          ? "Province/Territory"
+                          : "Region"
+                      }`}
+                      value={
+                        location ??
+                        (geographyType === "REGION" ? "ATLANTIC" : "AB")
+                      }
+                      onChange={(event) =>
+                        setFields({ ...fields, location: event.target.value })
+                      }
+                    >
+                      {geographyType === "PROVINCE_TERRITORY" && (
+                        <>
+                          <option value="AB">Alberta</option>
+                          <option value="BC">British Columbia</option>
+                          <option value="MB">Manitoba</option>
+                          <option value="NB">New Brunswick</option>
+                          <option value="NL">Newfoundland and Labrador</option>
+                          <option value="NS">Nova Scotia</option>
+                          <option value="NT">Northwest Territories</option>
+                          <option value="NU">Nunavut</option>
+                          <option value="ON">Ontario</option>
+                          <option value="PE">Prince Edward Island</option>
+                          <option value="QC">Quebec</option>
+                          <option value="SK">Saskatchewan</option>
+                          <option value="YT">Yukon</option>
+                        </>
+                      )}
+                      {geographyType === "REGION" && (
+                        <>
+                          <option value="ATLANTIC">Atlantic</option>
+                          <option value="PRAIRIE">Prairie</option>
+                          <option value="TERRITORIES">Territories</option>
+                        </>
+                      )}
+                    </Select>
+                  </FormControl>
+                )}
+              </HStack>
+            </VStack>
+
+            <VStack align="start">
+              <HStack>
+                <Icon as={CgHashtag} fontSize="xl" />
+                <Heading size="md" display="flex" alignItems="center">
+                  Value
+                </Heading>
+              </HStack>
+              <NumberInput
+                value={year2}
+                onChange={(val) =>
+                  setFields({
+                    ...fields,
+                    year2: val as unknown as number,
+                  })
+                }
+                id="dp_date2"
+                precision={0}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </VStack>
+
+            <VStack align="start">
+              <HStack>
+                <Icon as={AiOutlineCalendar} fontSize="xl" />
+                <Heading size="md" display="flex" alignItems="center">
+                  Year
+                </Heading>
+              </HStack>
+
+              <HStack w="100%" m={1} align="center">
+                <FormControl isRequired>
+                  <ButtonGroup isAttached>
+                    <Button
+                      size="sm"
+                      onClick={() =>
+                        setFields({ ...fields, yearType: "SINGLE" })
+                      }
+                      isActive={yearType === "SINGLE"}
                     >
                       Single Year
                     </Button>
                     <Button
                       size="sm"
-                      onClick={() => setYearType("range")}
-                      isActive={yearType === "range"}
+                      onClick={() =>
+                        setFields({ ...fields, yearType: "RANGE" })
+                      }
+                      isActive={yearType === "RANGE"}
                     >
                       Range
                     </Button>
                   </ButtonGroup>
                 </FormControl>
                 <FormControl isRequired>
-                  <FormLabel htmlFor={"dp_date1"}>
-                    {yearType === "range" ? "From" : "Year"}
-                  </FormLabel>
-                  <NumberInput
-                    min={1900}
-                    max={3000}
-                    value={year1}
-                    onChange={(val) => setYear1(val as unknown as number)}
-                    id="dp_date1"
-                    precision={0}
-                  >
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </FormControl>
-                {yearType === "range" && (
-                  <FormControl>
-                    <FormLabel htmlFor="dp_date2">{"To"}</FormLabel>{" "}
+                  <HStack>
+                    <FormLabel htmlFor={"dp_date1"}>
+                      {yearType === "RANGE" ? "From" : "Year"}
+                    </FormLabel>
                     <NumberInput
                       min={1900}
                       max={3000}
-                      value={year2}
-                      onChange={(val) => setYear2(val as unknown as number)}
-                      id="dp_date2"
+                      value={year1}
+                      onChange={(val) =>
+                        setFields({
+                          ...fields,
+                          year1: val as unknown as number,
+                        })
+                      }
+                      id="dp_date1"
                       precision={0}
                     >
                       <NumberInputField />
@@ -228,26 +311,53 @@ export function AddDataPointModal({
                         <NumberDecrementStepper />
                       </NumberInputStepper>
                     </NumberInput>
+                  </HStack>
+                </FormControl>
+                {yearType === "RANGE" && (
+                  <FormControl isRequired>
+                    <HStack>
+                      <FormLabel htmlFor="dp_date2">{"To"}</FormLabel>{" "}
+                      <NumberInput
+                        min={1900}
+                        max={3000}
+                        value={year2}
+                        onChange={(val) =>
+                          setFields({
+                            ...fields,
+                            year2: val as unknown as number,
+                          })
+                        }
+                        id="dp_date2"
+                        precision={0}
+                      >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                    </HStack>
                   </FormControl>
                 )}
-              </Stack>
-            </Box>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              type="submit"
-              colorScheme="green"
-              mr={3}
-              float="right"
-              // onClick={handleSubmit}
-            >
-              Save
-            </Button>
-            <Button colorScheme="red" onClick={onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </form>
+              </HStack>
+            </VStack>
+            {/* </Box> */}
+          </VStack>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            type="submit"
+            colorScheme="green"
+            mr={3}
+            float="right"
+            // onClick={handleSubmit}
+          >
+            Save
+          </Button>
+          <Button colorScheme="red" onClick={onClose}>
+            Cancel
+          </Button>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );

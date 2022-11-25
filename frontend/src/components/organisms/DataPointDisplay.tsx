cf5babeea094b24b1f7c4c3457a032bf1bd0cb5c
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-11-25 10:08:53 -0800

    Add review table

---
+++ frontend/src/components/organisms/DataPointDisplay.tsx
@@ -0,0 +1,49 @@
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { DataPoint } from "../../utils/types";
+
+const DataPointDisplay = ({ dataPoints }: { dataPoints: DataPoint[] }) => {
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption></TableCaption>
+        <Thead>
+          <Tr>
+            <Th isNumeric>#</Th>
+            <Th>Geography</Th>
+            <Th>Location</Th>
+            <Th isNumeric>Value</Th>
+            <Th>Unit</Th>
+            <Th>Data Quality</Th>
+            <Th>Year</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {dataPoints.map((dataPoint, idx) => (
+            <Tr key={dataPoint.uuid}>
+              <Td isNumeric>{idx + 1}</Td>
+              <Td>{dataPoint.geography}</Td>
+              <Td>{dataPoint.country}</Td>
+              <Td isNumeric>{dataPoint.value}</Td>
+              <Td>{dataPoint.valueUnit}</Td>
+              <Td>{dataPoint.dataQuality}</Td>
+              <Td>
+                {dataPoint.singleYearTimeframe ?? dataPoint.multiYearTimeframe}
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default DataPointDisplay;

2bbf886bf87cd1ab52d2600beb577299a12f73ed
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-11-25 10:09:02 -0800

    Review page

--- frontend/src/components/molecules/ReviewSubmit.tsx
+++ frontend/src/components/molecules/ReviewSubmit.tsx
@@ -1,5 +1,12 @@
 import { useMutation } from "@apollo/client";
-import { Button, Heading, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  StatDownArrow,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { categories, sub_categories } from "../../utils/constants";
 import { CREATE_INDICATOR } from "../../utils/graphql/mutations";
 import {
@@ -7,6 +14,7 @@ import {
   GET_INDICATORS_AND_IDS,
 } from "../../utils/graphql/queries";
 import { DataPoint } from "../../utils/types";
+import DataPointDisplay from "../organisms/DataPointDisplay";
 
 const ReviewSubmit = ({
   values,
@@ -41,10 +49,13 @@ const ReviewSubmit = ({
     }
   );
 
+  const [openTable, setOpenTable] = useState(false);
+
   return (
-    <VStack>
-      <Heading>Review and Submit</Heading>
-      {indicatorName ? (
+    <VStack align="start" spacing={8} w={["98%", "80%", "70%"]}>
+      <Heading alignSelf="center">Review and Submit</Heading>
+
+      <HStack spacing={6}>
         <Heading size="lg">
           You are adding{" "}
           <Heading as="span" color="blue.500" size="lg">
@@ -52,31 +63,47 @@ const ReviewSubmit = ({
           </Heading>{" "}
           data point{dataPoints.length === 1 ? "" : "s"} to
           <Heading as="span" size="lg" color="blue.500">
-            {` ${indicatorName}`}
+            {indicatorName
+              ? ` ${indicatorName}`
+              : " NULL - INDICATOR NOT SELECTED"}
           </Heading>
         </Heading>
-      ) : (
-        <Heading size="lg">
-          Please select an indicator in the first step
+        <Button
+          leftIcon={
+            <StatDownArrow
+              color={openTable ? "red.500" : "green.500"}
+              transform={openTable ? "rotate(180deg)" : "rotate(0deg)"}
+            />
+          }
+          onClick={() => setOpenTable(!openTable)}
+        >
+          Review Data
+        </Button>
+      </HStack>
+
+      {openTable && <DataPointDisplay dataPoints={dataPoints} />}
+
+      <VStack align="start">
+        <Heading fontSize="x-large">
+          Category:{" "}
+          <Heading fontSize="x-large" as="span" color="blue.500">
+            {categories.filter((c) => c.id === category)[0].label}
+          </Heading>
         </Heading>
-      )}
-      <Heading size="lg">
-        Category:{" "}
-        <Heading size="lg" as="span" color="blue.500">
-          {categories.filter((c) => c.id === category)[0].label}
+        <Heading fontSize="x-large">
+          Subcategory:{" "}
+          <Heading fontSize="x-large" as="span" color="blue.500">
+            {sub_categories.filter((c) => c.id === subCategory)[0].label}
+          </Heading>
         </Heading>
-      </Heading>
-      <Heading size="lg">
-        Subcategory:{" "}
-        <Heading size="lg" as="span" color="blue.500">
-          {sub_categories.filter((c) => c.id === subCategory)[0].label}
-        </Heading>
-      </Heading>
+      </VStack>
+
       {loading && <Heading size="md">Loading...</Heading>}
       {error && <Heading size="md">Error: {error.message}</Heading>}
       {data && <Heading size="md">Success!</Heading>}
       {!data && !error && (
         <Button
+          alignSelf="end"
           disabled={dataPoints.length === 0}
           colorScheme="green"
           isDisabled={loading}

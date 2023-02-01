e0346e28305b1df300109ab45a0a9525c14bc37f
Mike Williamson <mike@korora.ca>
2023-02-01 08:55:45 -0500

    remove reference to frontend files from server (#14)
    This commit removes references to the frontends static files from the API. The
    two services are already separate, and this config is clearly left over from
    some experiment from days gone by. One of the sacred tenants of microservices
    is that they are independently deployable, and it's good to remove these to
    keep the system boundaries clear.
    
    Co-authored-by: Eduardo Szeckir <50898635+szeckirjr@users.noreply.github.com>

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'graphene_django',
 ]
@@ -61,7 +60,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '..', 'frontend', 'build')],
+        'DIRS': [os.path.join(BASE_DIR, '..')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,12 +125,6 @@ USE_L10N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:8000']
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'frontend', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

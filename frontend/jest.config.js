b0bdc75ba6323971f51afdeb2cdb85f7dfc10ec1
Eduardo Szeckir <50898635+szeckirjr@users.noreply.github.com>
2023-02-13 11:17:23 -0800

    Frontend Testing (#15)
    * initial frontend testing configs
    
    * some more changes
    
    * add more variety of tests for demo
    
    * fixing dependencies

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};

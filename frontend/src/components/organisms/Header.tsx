22d3d8c41316366f1abf144530784e26323e817d
Sami Atoui <atoui79@gmail.com>
2023-05-09 13:47:06 -0500

    PHAC Component done

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -1,14 +1,25 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, useColorMode } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+import { Phacsignature } from "../atoms/Phacsignature";
 
 export function Header(): JSX.Element {
+  const { colorMode } = useColorMode();
+
   return (
     <>
       <HStack w="100%" display="flex" justify="space-between" p={5}>
         <Image maxWidth="400px" width="80%" src="/static/images/logo.svg" />
+        
+
         {/* TODO: Implement language switcher */}
         <ColorModeSwitcher />
+
       </HStack>
+      
+      <Phacsignature
+          textColor={colorMode === "dark" ? "white" : "black"}
+          variant="color"
+        />
       <hr
         style={{
           marginTop: 10,

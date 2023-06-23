9d3e4f1620dda3a89d686254a95fc46a0896d868
AlexCLeduc <AlexCLeduc@users.noreply.github.com>
2023-06-23 16:01:15 -0400

    Remove old configuration (#44)
    * rm old configuration (frontend, k8s, etc.)
    * update docs
    * copy dockerfile/entrypoint from itap/data-catalog

---
+++ server/entrypoint.prod.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "Starting SSH server..."
+/usr/sbin/sshd
+
+echo "Generating static files..."
+python manage.py collectstatic --no-input
+
+if [[ ! -z "$DB_HOST" && ! -z "$DB_PORT" ]]; then
+  echo "Waiting for postgres ($DB_HOST:$DB_PORT)..."
+
+  while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+  done
+  sleep 1
+
+  echo "PostgreSQL started"
+  echo "applying migrations..."
+  python manage.py migrate
+  echo "migrations applied"
+
+fi
+
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+exec su-exec app "$@"
--- server/server.py
+++
@@ -1,6 +0,0 @@
-from waitress import serve
-
-from server.wsgi import application
-
-if __name__ == "__main__":
-    serve(application, listen="*:8080")

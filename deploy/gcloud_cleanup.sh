bb07b9e4605857a45bf6768df40a1d3b7d1be8e0
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-07-05 09:33:57 -0400

    Minor fixes in cloud cleanup and prod env file scripts

--- deploy/gcloud_cleanup.sh
+++ deploy/gcloud_cleanup.sh
@@ -3,21 +3,20 @@ set -o errexit
 set -o pipefail
 set -o nounset
  
-
-
 # ----- Get configuration variables + secrets helpers -----
 source ./deploy/gcloud_env_vars.sh
 
+# NOTE: using `|| :`, where : is effectively a bash no-op, to prevent errors in certain lines from exiting the script
 
 # ----- ARTIFACT REGISTRY -----
 # TODO back up first (move X latest images to cold storage?), set some TTL on that?
 # TODO revert permissions to default, disable API?
-gcloud artifacts repositories delete ${ARTIFACT_REGISTRY_REPO} --location ${PROJECT_REGION}
+gcloud artifacts repositories delete ${ARTIFACT_REGISTRY_REPO} --location ${PROJECT_REGION} || :
 
 # ----- CLOUD BUILD ----
 # TODO maybe also find and delete the project's repository connection object?
 # TODO revert permissions to default, disable API?
-gcloud builds triggers delete github ${BUILD_CLOUD_BUILD_TRIGGER_NAME}
+gcloud builds triggers delete github ${BUILD_CLOUD_BUILD_TRIGGER_NAME} || :
 
 # ----- CLOUD RUN -----
 # TODO revert permissions to default, disable API?
@@ -29,7 +28,7 @@ gcloud builds triggers delete github ${BUILD_CLOUD_BUILD_TRIGGER_NAME}
 # ----- CLOUD SQL -----
 # TODO back up first?
 # TODO revert permissions to default, disable API?
-gcloud sql instances delete ${DB_INSTANCE_NAME}
+gcloud sql instances delete ${DB_INSTANCE_NAME} || :
 
 # ----- SECRET MANAGER -----
 SKEY_ENV_VAR_NAMES=$(env -0 | cut -z -f1 -d= | tr '\0' '\n' | grep "^SKEY_")
--- deploy/make_prod_env_file.sh
+++ deploy/make_prod_env_file.sh
@@ -16,13 +16,14 @@ cat <<EOT >> ${PROD_ENV_FILE}
 DB_NAME=$(get_secret ${SKEY_DB_NAME})
 DB_USER=$(get_secret ${SKEY_DB_USER})
 DB_PASSWORD=$(get_secret ${SKEY_DB_USER_PASSWORD})
-DB_HOST=$(gcloud sql instances describe ${SKEY_DB_INSTANCE_NAME} --format=json | jq -r '.ipAddresses[] | select(.type=="PRIMARY") | .ipAddress')
+DB_HOST=$(gcloud sql instances describe $(get_secret ${SKEY_DB_INSTANCE_NAME}) --format=json | jq -r '.ipAddresses[] | select(.type=="PRIMARY") | .ipAddress')
 DB_PORT=5432
 
 SECRET_KEY=$(get_secret ${SKEY_DJANGO_SECRET_KEY})
 
 # This won't work on the first Cloud Run deploy, as the service won't have a URL yet
-ALLOWED_HOSTS=[$(gcloud run services describe ${PROJECT_SERVICE_NAME} --platform managed --region REGION --format "value(status.url)")]
+# TODO falling back to wildcard if can't get the service URL
+ALLOWED_HOSTS=$(gcloud run services describe ${PROJECT_SERVICE_NAME} --platform managed --region REGION --format "value(status.url)" || echo *)
 
-$(if [[ ! $PROJECT_IS_USING_WHITENOISE ]]; then; echo MEDIA_BUCKET_NAME=$(get_secret ${SKEY_MEDIA_BUCKET_NAME}); fi)
+$(if [[ ! $PROJECT_IS_USING_WHITENOISE ]]; then echo MEDIA_BUCKET_NAME=$(get_secret ${SKEY_MEDIA_BUCKET_NAME}); fi)
 EOT

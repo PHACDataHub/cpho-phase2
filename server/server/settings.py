e4f0c7b8e7d33dcd37b89306f9aa055fe6f23efc
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-07-07 12:39:42 -0400

    Disable CSRF for now, since it's not working on the cloud run instance

--- server/server/settings.py
+++ server/server/settings.py
@@ -132,7 +132,7 @@ MIDDLEWARE = configure_middleware(
         "django.middleware.common.CommonMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
         "django.middleware.common.CommonMiddleware",
-        "django.middleware.csrf.CsrfViewMiddleware",
+        # "django.middleware.csrf.CsrfViewMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",

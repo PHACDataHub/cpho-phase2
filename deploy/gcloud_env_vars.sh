40fc32743ad47b477655512b3622a0375c292c8e
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-07-13 14:53:09 -0400

    Set GITHUB_MAIN_BRANCH_NAME back to cloud-run-deployment for further testing

--- deploy/gcloud_env_vars.sh
+++ deploy/gcloud_env_vars.sh
@@ -17,7 +17,7 @@ export PROJECT_REGION=northamerica-northeast1
 export BUILD_GITHUB_REPO_NAME=cpho-phase2
 export BUILD_GITHUB_REPO_OWNER=PHACDataHub
 
-export GITHUB_MAIN_BRANCH_NAME=main
+export GITHUB_MAIN_BRANCH_NAME=cloud-run-deployment # main
 
 # NOTE: if PROJECT_IS_USING_WHITENOISE is true then no cloud storage will be created for the project
 export PROJECT_IS_USING_WHITENOISE=true

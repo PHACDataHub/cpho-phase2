2b01ab3a92d28eb95ddb197179914e8981092f9c
Lila Kelland <lila.kelland@gmail.com>
2023-07-18 09:18:17 -0400

    added axes to AUTHENTICATION_BACKENDS

--- server/server/settings.py
+++ server/server/settings.py
@@ -222,6 +222,7 @@ if IS_RUNNING_TESTS:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTHENTICATION_BACKENDS = [
+    "axes.backends.AxesStandaloneBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
 AUTH_USER_MODEL = "cpho.User"

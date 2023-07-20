e4bc31fb377761490794905868ca6dbb148d758b
Lila Kelland <lila.kelland@gmail.com>
2023-07-20 09:36:42 -0400

    added commit sha back in as not being fed to docker compose, removed codecov from deploy scripts.

--- deploy/make_cloud_build_env_file.sh
+++ deploy/make_cloud_build_env_file.sh
@@ -30,8 +30,6 @@ bash_escape "COMMIT_SHA=${commit_sha}" >> "${env_file}"
 
 bash_escape "IMAGE_NAME_FOR_RUN=${BUILD_CLOUD_RUN_IMAGE_NAME}:${commit_sha}" >> "${env_file}"
 
-bash_escape "CODECOV_TOKEN=$(gcloud secrets versions access 1 --secret=$SKEY_CODECOV_TOKEN)" >> "${env_file}"
-
 # re-exports from gcloud_env_vars.sh
 bash_escape "GITHUB_MAIN_BRANCH_NAME=${GITHUB_MAIN_BRANCH_NAME}" >> "${env_file}"
 bash_escape "PROJECT_SERVICE_NAME=${PROJECT_SERVICE_NAME}" >> "${env_file}"

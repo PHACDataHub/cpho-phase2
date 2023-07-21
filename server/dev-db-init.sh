372b45eebcf77de4e53bf8ace557486581f6a2db
Lila Kelland <lila.kelland@gmail.com>
2023-07-21 17:27:27 -0400

    renamed docker compose for changed use, implimented change to have tests error code fed back to testing cloudbuild step.  Fixed script to be used locally and in cloudbuild. This still uses the sql file - need to figure out why the dev_db_init script isn't connecting to db

---
+++ server/dev-db-init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# ----- Get environment variables  -----
+source $(dirname "${BASH_SOURCE[0]}")/.env.dev
+
+# Create "cpho_db_user" role if not exists
+if ! psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -tAc "SELECT 1 FROM pg_roles WHERE rolname='cpho_db_user'" | grep -q 1; then
+    # Role does not exist - create it
+    psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" <<-EOSQL
+        CREATE ROLE cpho_db_user WITH LOGIN PASSWORD '';
+        ALTER ROLE cpho_db_user CREATEDB;
+EOSQL
+# echo "Created cpho_db_user role."
+fi
+
+# Create "cpho_dev_db" if not exists
+if ! psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -lqt | cut -d \| -f 1 | grep -qw "cpho_dev_db"; then
+    # Database does not exist, so create it
+    psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" <<-EOSQL
+        CREATE DATABASE cpho_dev_db WITH OWNER cpho_db_user;
+EOSQL
+# echo "Created cpho_dev_db database."
+fi

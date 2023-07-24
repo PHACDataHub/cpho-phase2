d16d3ed8307538382a22e82c7a66a2912c146d22
Lila Kelland <lila.kelland@gmail.com>
2023-07-24 10:28:21 -0400

    replaced user and db set up with script from docker site

--- server/dev-db-init.sh
+++ server/dev-db-init.sh
@@ -6,27 +6,33 @@ set -o nounset
 # ----- Get environment variables  -----
 source $(dirname "${BASH_SOURCE[0]}")/.env.dev
 
-# cat .env.dev
-
-# Create "cpho_db_user" role if not exists
-if ! psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -tAc "SELECT 1 FROM pg_roles WHERE rolname='cpho_db_user'" | grep -q 1; then
-    # Role does not exist - create it
-    psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" <<-EOSQL
-        CREATE ROLE cpho_db_user WITH LOGIN PASSWORD '';
-        ALTER ROLE cpho_db_user CREATEDB;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER $DB_USER;
+	CREATE DATABASE $TEST_DB_NAME;
+	GRANT ALL PRIVILEGES ON DATABASE $TEST_DB_NAME TO docker;
 EOSQL
-    echo "Creating cpho_db_user role"
-else 
-    echo "cpho_db_user role already exists."
-fi
 
-# Create "cpho_dev_db" if not exists
-if ! psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -lqt | cut -d \| -f 1 | grep -qw "cpho_dev_db"; then
-    # Database does not exist, so create it
-    psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" <<-EOSQL
-        CREATE DATABASE cpho_dev_db WITH OWNER cpho_db_user;
-EOSQL
-    echo "Created cpho_dev_db database."
-else
-    echo "cpho_dev_db database  already exists."
-fi
+# # cat .env.dev
+
+# # Create "cpho_db_user" role if not exists
+# if ! psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -tAc "SELECT 1 FROM pg_roles WHERE rolname='cpho_db_user'" | grep -q 1; then
+#     # Role does not exist - create it
+#     psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" <<-EOSQL
+#         CREATE ROLE cpho_db_user WITH LOGIN PASSWORD '';
+#         ALTER ROLE cpho_db_user CREATEDB;
+# EOSQL
+#     echo "Creating cpho_db_user role"
+# else 
+#     echo "cpho_db_user role already exists."
+# fi
+
+# # Create "cpho_dev_db" if not exists
+# if ! psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -lqt | cut -d \| -f 1 | grep -qw "cpho_dev_db"; then
+#     # Database does not exist, so create it
+#     psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" <<-EOSQL
+#         CREATE DATABASE cpho_dev_db WITH OWNER cpho_db_user;
+# EOSQL
+#     echo "Created cpho_dev_db database."
+# else
+#     echo "cpho_dev_db database  already exists."
+# fi

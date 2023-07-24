6e462e611e565514b075dc0d467de02d35f265d7
Lila Kelland <lila.kelland@gmail.com>
2023-07-24 10:38:50 -0400

    set postgres_user to postgres

--- server/dev-db-init.sh
+++ server/dev-db-init.sh
@@ -5,8 +5,9 @@ set -o nounset
 
 # ----- Get environment variables  -----
 source $(dirname "${BASH_SOURCE[0]}")/.env.dev
+POSTGRES_USER=postgres
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"  <<-EOSQL
 	CREATE USER $DB_USER;
 	CREATE DATABASE $TEST_DB_NAME;
 	GRANT ALL PRIVILEGES ON DATABASE $TEST_DB_NAME TO $DB_USER;

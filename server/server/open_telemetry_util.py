4bec8e20e7c559b9821359351cedc45d6ffce805
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-07-25 17:12:55 -0400

    Add basic OpenTelemetry instrumentation

---
+++ server/server/open_telemetry_util.py
@@ -0,0 +1,36 @@
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+)
+
+from server.settings_util import get_project_config
+
+
+def instrument_app():
+    config = get_project_config()
+    IS_LOCAL_DEV = config("IS_LOCAL_DEV", cast=bool, default=False)
+
+    if IS_LOCAL_DEV:
+        span_exporter = ConsoleSpanExporter()
+    else:
+        span_exporter = CloudTraceSpanExporter(
+            project_id="TODO",
+        )
+
+    # a BatchSpanProcessor is better for performance but uses a background process,
+    # which would require tricky and careful management in Cloud Run. Even in the best case,
+    # I expect the necessary tricks would still result in the occasional dropped trace.
+    # BatchSpanProcessor also requires extra configuration when combined with gunicorn's process forking
+    span_processor = SimpleSpanProcessor(span_exporter)
+
+    tracer_provider = TracerProvider(active_span_processor=span_processor)
+
+    DjangoInstrumentor().instrument(
+        tracer_provider=tracer_provider,
+        meter_provider=None,  # TODO
+        is_sql_commentor_enabled=True,
+    )

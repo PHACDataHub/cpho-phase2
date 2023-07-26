2a4502ece3615bb8859c1ed3ad92079ca383fdd9
Sarar <galaxyraad@gmail.com>
2023-07-26 11:19:25 -0400

    added plpaceholder rules

--- server/cpho/rules.py
+++ server/cpho/rules.py
@@ -14,20 +14,64 @@ def is_program(user):
     return user.username == "program"
 
 
-def is_lead_for_branch(user):
+def is_branch_lead(user):
+    return True
+
+
+def is_branch_user(user):
+    return True
+
+
+def is_branch_lead_or_user(user):
+    return is_branch_user(user) or is_branch_lead(user)
+
+
+def is_indicator_lead(user, indicator):
     return True
 
 
 def indicator_of_users_branch(user, indicator):
-    """
-    TODO: implement this rule
-    """
     return True
 
 
 def can_edit_indicator(user, indicator):
     return (
-        user.is_admin
-        or user.is_hso
-        or (user.is_program and indicator_of_users_branch(user, indicator))
+        is_admin(user)
+        or is_hso(user)
+        or (is_program(user) and is_indicator_lead(user, indicator))
+        or (
+            is_program(user)
+            and is_branch_lead(user)
+            and indicator_of_users_branch(user, indicator)
+        )
     )
+
+
+def can_submit_as_program(user, indicator):
+    return (
+        is_admin(user)
+        or is_hso(user)
+        or (is_program(user) and is_indicator_lead(user, indicator))
+        # or (
+        #     is_program(user)
+        #     and is_branch_lead(user)
+        #     and indicator_of_users_branch(user, indicator)
+        # )
+    )
+
+
+def can_view_indicator(user, indicator):
+    return (
+        is_admin(user)
+        or is_hso(user)
+        or (is_program(user) and is_indicator_lead(user, indicator))
+        or (
+            is_program(user)
+            and is_branch_lead_or_user(user)
+            and indicator_of_users_branch(user, indicator)
+        )
+    )
+
+
+def can_submit_as_hso(user):
+    return is_admin(user) or is_hso(user)

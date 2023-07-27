06ebfc41db74b4d100a4bf874f592ce1b7a656ac
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-07-27 12:17:12 -0400

    Adjustments and comments in gunicorn.conf.py. Ended up moving instrumentation back in to server/wsgi.py, because the post_fork hook call shouldn't be necessary with SimpleSpanProcessor and isn't compatible with the GCP recommended preload_app gunicorn setting

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,20 +1,23 @@
-import multiprocessing
 import os
 
-from server.open_telemetry_util import instrument_app
+# See https://cloud.google.com/run/docs/tips/python#optimize_gunicorn
 
+# PORT env var is set by Cloud Run
 PORT = os.getenv("PORT", "8080")
-
 bind = [f"0.0.0.0:{PORT}"]
-workers = 1
-threads = multiprocessing.cpu_count() * 2 + 1
-timeout = 0
+
+# Note: the generally recommended formula is `workers + threads = multiprocessing.cpu_count() * 2 + 1`,
+# not totally clear why these google doc tips use 8 threads. Haven't confirmed that `multiprocessing.cpu_count()`
+# is accurate in Cloud Run serverless environment anyway (I know JS libraries used to incorrectly detect vCPUs all the time)
+workers = 1  # increase this, and possibly decrease threads, if we ever configure a higher CPU count for Cloud Run
+threads = 8
+
+timeout = 0  # from the GCP docs: "timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling."
+
+# Preloading is recommended by the GCP docs, with caveats; gotta be aware of what might be preloaded/resources used in the process
+# Warning: preloading loads the app before gunicorn forks. This may conflict with potential uses of gunicorn hooks like post_fork.
+# E.g. if we use OpenTelemetry's background process based BatchSpanProcessor, then we need to insturment the app from the post_fork hook,
+# to get different background processses per processor. Instrumentation needs to happen before the app launches, so in this case preload_app can't be used
+preload_app = True
+
 wsgi_app = "server.wsgi:application"
-
-
-def post_fork(server, worker):
-    # Need to set "DJANGO_SETTINGS_MODULE" before instrumenting, and it's suggested to
-    # instrument first thing after setting it, so this is the most sensible place for it
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-
-    instrument_app()

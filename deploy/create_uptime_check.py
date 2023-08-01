41013e0a54c8920d3487a85aaaa337420c3693cb
Lila Kelland <lila.kelland@gmail.com>
2023-08-01 14:20:04 -0400

    Using API/ python to create uptime check - will need to pull env_vars from other deploy scripts and create alert

--- deploy/create_uptime_check.py
+++ deploy/create_uptime_check.py
@@ -1,4 +1,41 @@
+# Be sure to auth login before running this: gcloud auth application-default login
+# Also activate monitoring API & add role to m
+
+# from google.cloud import monitoring_v3
+from __future__ import print_function
+
+import argparse
+import os
+import pprint
+
+import tabulate
+from google.cloud import monitoring_v3
+from google.cloud.monitoring_v3.services.uptime_check_service import pagers
+from google.cloud.monitoring_v3.types import uptime
+from google.protobuf import field_mask_pb2
+
+# # from google.cloud.monitoring_v3 import MonitoredResource
+# # from google.protobuf import duration_pb2
+
+#TODO - pull these in from env_vars
+PROJECT_ID="phx-01h4rr1468rj3v5k60b1vserd3"
+PROJECT_SERVICE_NAME="cpho-phase2"
+PROJECT_REGION="northamerica-northeast1"
+
+display_name = "HoPiC Uptime Check test get 1"
+parent = f'projects/{PROJECT_ID}'
+
 # reference: https://cloud.google.com/monitoring/docs/samples/monitoring-uptime-check-create?hl=en#monitoring_uptime_check_create-python
+# and here https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/monitoring/snippets/v3/uptime-check-client/snippets.py
+
+# alerts https://github.com/GoogleCloudPlatform/python-docs-samples/tree/84816c5cc5b09092c2df795f367abba39c147a24/monitoring/snippets/v3/alerts-client
+# https://cloud.google.com/monitoring/uptime-checks/uptime-alerting-policies?hl=en
+
+#reference https://cloud.google.com/monitoring/api/ref_v3/rpc/google.monitoring.v3#google.monitoring.v3.UptimeCheckConfig
+
+# https://cloud.google.com/monitoring/api/ref_v3/rpc/google.monitoring.v3#google.monitoring.v3.UptimeCheckService.CreateUptimeCheckConfig
+
+
 def create_uptime_check_config_get(
     project_id: str, host_name: str = None, display_name: str = None
     ) -> uptime.UptimeCheckConfig:
@@ -12,16 +49,18 @@ def create_uptime_check_config_get(
     Returns:
         A structure that describes a new created uptime check
     """
-    project_id = "phx-01h4rr1468rj3v5k60b1vserd3"
-    display_name = "HoPiC Uptime Check"
-    host_name = " "
 
     config = monitoring_v3.UptimeCheckConfig()
-    config.display_name = display_name or "New GET uptime check"
+    config.display_name = display_name 
     config.monitored_resource = {
-        # "type": "uptime_url",
-        # "labels": {"host": host_name or "example.com"},
-        "type": "Cloud Run Service"
+        "type": "cloud_run_revision",
+        "labels": {
+            "project_id": PROJECT_ID,
+            "service_name": PROJECT_SERVICE_NAME,
+            "location": PROJECT_REGION,
+            # revision_name: Name of the monitored revision.
+            # configuration_name: Name of the configuration which created the monitored revision.
+        },
 
     }
     config.http_check = {
@@ -29,62 +68,20 @@ def create_uptime_check_config_get(
         "path": "/",
     }
     config.timeout = {"seconds": 10}
-    config.period = {"seconds": 300}
+    config.period = {"seconds": 900}
 
     client = monitoring_v3.UptimeCheckServiceClient()
     new_config = client.create_uptime_check_config(
-        request={"parent": project_id, "uptime_check_config": config}
+        request={"parent": parent, "uptime_check_config": config}
     )
     pprint.pprint(new_config)
     return new_config
 
 
-def create_uptime_check_config_post(
-    project_id: str, host_name: str = None, display_name: str = None
-) -> uptime.UptimeCheckConfig:
-    """Creates a new uptime check configuration
+create_uptime_check_config_get(project_id=PROJECT_ID, display_name=display_name)
+print("SUCESS YAY!!!!!!")
 
-    Args:
-        project_id: Google Cloud project id where the uptime check is created
-        host_name: An example label's value for the "host" label
-        display_name: A human friendly name of the configuration
-
-    Returns:
-        A structure that describes a new created uptime check
-    """
-    config = monitoring_v3.UptimeCheckConfig()
-    config.display_name = display_name or "New POST uptime check"
-    config.monitored_resource = {
-        "type": "uptime_url",
-        "labels": {"host": host_name or "example.com"},
-    }
-    config.http_check = {
-        "request_method": monitoring_v3.UptimeCheckConfig.HttpCheck.RequestMethod.POST,
-        "content_type": monitoring_v3.UptimeCheckConfig.HttpCheck.ContentType.URL_ENCODED,
-        # "body": "foo=bar".encode("utf-8"),
-        "path": "/",
-        # "port": 80,
-    }
-    config.timeout = {"seconds": 10}
-    config.period = {"seconds": 300}
-
-    #want log check failures 
-    log_check_failures = True
-    response_timeout = 10s
-
-    client = monitoring_v3.UptimeCheckServiceClient()
-    new_config = client.create_uptime_check_config(
-        request={"parent": project_id, "uptime_check_config": config}
-    )
-    pprint.pprint(new_config)
-    return new_config
-
-#Create alert 
-Name = "HoPiC Uptime Alert uptime failure"
-duration = 60s
-notification channels - google cloud console moble UserWarning
-
-Target
-Response
-Alert 
-
+# #Create alert 
+# Name = "HoPiC Cloud Run Outage Alert"
+# duration = 900s
+# notification channels - google cloud console moble UserWarning

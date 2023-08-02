5c8234ad199aa497e7582c8c2491e5fe70f6d4fd
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-08-02 11:24:18 -0400

    OpenTelemetry BatchSpanProcessor seems far better for performance. Move instrumentation to a gunicorn post_fork hook, and disable preload_app, to support its use

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,5 +1,7 @@
 import os
 
+from server.open_telemetry_util import instrument_app_for_open_telemetry
+
 # See https://cloud.google.com/run/docs/tips/python#optimize_gunicorn
 
 # PORT env var is set by Cloud Run
@@ -15,9 +17,19 @@ threads = 8
 timeout = 0  # from the GCP docs: "timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling."
 
 # Preloading is recommended by the GCP docs, with caveats; gotta be aware of what might be preloaded/resources used in the process
-# Warning: preloading loads the app before gunicorn forks. This may conflict with potential uses of gunicorn hooks like post_fork.
+# Warning: preloading loads the app before gunicorn forks, meaning it's NOT compatible with situations where some initialization must occur in a post_fork hook
 # E.g. if we use OpenTelemetry's background process based BatchSpanProcessor, then we need to insturment the app from the post_fork hook,
 # to get different background processses per processor. Instrumentation needs to happen before the app launches, so in this case preload_app can't be used
-preload_app = True
+# preload_app = True
+
+
+def post_fork(server, worker):
+    # When using OpenTelemetry's background process based BatchSpanProcessor, insturmentation must occur post process fork. Pre-fork instrumentation
+    # would result in each app process trying to share the same BatchSpanProcessor background process, which would not be reliable.
+    # If NOT using BatchSpanProcessor (likely a bad idea, it's much more performant at run time) you can move these lines to wsgi.py and enable preload_app
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+    instrument_app_for_open_telemetry()
+
 
 wsgi_app = "server.wsgi:application"

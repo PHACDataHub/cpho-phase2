b4939861c28b6ff27385034d8734c60578e9c657
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-08-18 15:19:37 -0400

    Google's webhook based monitoring notification channels are simply not compatable with Slack webhooks. Switched to a slack bot auth token (wasn't hard to give the existing webhook app permissions to also post as a bot). Rewrite and document the AlertPolicy... the docs for this were an unfriendly slog, so hopefully my understanding is finally deep enough that this works the first time around

--- deploy/gcloud_env_vars.sh
+++ deploy/gcloud_env_vars.sh
@@ -26,6 +26,8 @@ export APP_HEALTHCHECK_ROUTE=/healthcheck
 # NOTE: if PROJECT_IS_USING_WHITENOISE is true then no cloud storage will be created for the project
 export PROJECT_IS_USING_WHITENOISE=true
 
+export SLACK_ALERTING_CHANNEL_NAME="#cpho-hopic-prod-alerts"
+
 export TEST_COVERAGE_BUCKET_NAME=hopic-test-coverage-reports
 export TEST_COVERAGE_THRESHOLD=80
 export TEST_DELTA_THRESHOLD=-5  #current commit test coverage minus last commit on main's coverage
@@ -91,7 +93,7 @@ export VPC_CONNECTOR_RANGE="10.8.0.0/28" # must be /28 and unused. 10.8.0.0 shou
 export SKEY_DB_ROOT_PASSWORD=db_root_password
 export SKEY_DB_USER_PASSWORD=db_user_password
 export SKEY_PROD_ENV_FILE=django_production_env
-export SKEY_UPTIME_ALERT_SLACK_WEBHOOK=uptime_alert_slack_webhook
+export SKEY_PROD_SLACK_ALERTING_APP_TOKEN=slack_alerting_app_token
 
 
 set_secret () {
--- deploy/pulumi/uptime_monitoring/uptime_monitoring.ts
+++ deploy/pulumi/uptime_monitoring/uptime_monitoring.ts
@@ -1,4 +1,5 @@
 import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
 import { execSync } from "child_process";
 
 const env = (env_name: string) => {
@@ -13,23 +14,27 @@ const env = (env_name: string) => {
   return env_var;
 };
 
+// asserting SKEY_SLACK_MONITOR_ALERTING_WEBHOOK exists as an env var via `env()`, but not interpolating the value in to the exec()
+// command string directly. Potentially safer to let the exec child process shell handle that via the bash best-practice "${...}" pattern
+env("SKEY_PROD_SLACK_ALERTING_APP_TOKEN");
+const slack_app_token = execSync(
+  'gcloud secrets versions access latest --secret "${SKEY_PROD_SLACK_ALERTING_APP_TOKEN}"',
+  { shell: "/bin/bash", encoding: "utf8" }
+);
+
 new gcp.Provider(env("PROJECT_SERVICE_NAME"), {
   project: env("PROJECT_ID"),
   region: env("PROJECT_REGION"),
 });
 
-// asserting SKEY_SLACK_MONITOR_ALERTING_WEBHOOK exists as an env var via `env()`, but not interpolating the value in to the exec()
-// command string directly. Potentially safer to let the exec child process shell handle that via the bash best-practice "${...}" pattern
-env("SKEY_UPTIME_ALERT_SLACK_WEBHOOK");
-const slack_alerting_webhook = execSync(
-  'gcloud secrets versions access latest --secret "${SKEY_UPTIME_ALERT_SLACK_WEBHOOK}"',
-  { shell: "/bin/bash", encoding: "utf8" }
-);
-
+const uptime_check_period: 60 | 300 | 600 | 900 = 60;
 const healthcheckRouteUptimeCheck = new gcp.monitoring.UptimeCheckConfig(
   `${env("PROJECT_SERVICE_NAME")}-https-uptime-check`,
   {
-    displayName: "Uptime check over https against the app's /healthcheck route",
+    project: env("PROJECT_ID"),
+    displayName: `Uptime check over https against the ${env(
+      "PROJECT_SERVICE_NAME"
+    )} app's /healthcheck route`,
     monitoredResource: {
       labels: {
         host: env("DNS_DOMAIN"),
@@ -49,19 +54,22 @@ const healthcheckRouteUptimeCheck = new gcp.monitoring.UptimeCheckConfig(
         },
       ],
     },
-    period: "900s",
+    period: pulumi.interpolate`${uptime_check_period}s`,
     timeout: "10s",
+    selectedRegions: ["usa-virginia", "usa-oregon", "usa-iowa"],
   }
 );
 
 const slackNotificationChannel = new gcp.monitoring.NotificationChannel(
-  `${env("PROJECT_SERVICE_NAME")}-slack-webhook-alert`,
+  `${env("PROJECT_SERVICE_NAME")}-slack-app-alert`,
   {
-    type: "webhook_tokenauth",
-    displayName: `${env("PROJECT_SERVICE_NAME")} Webhook Alert Channel`,
-    description: "Webhook notification channel for Cloud Run Outages",
+    project: env("PROJECT_ID"),
+    type: "slack",
+    displayName: `${env("PROJECT_SERVICE_NAME")} Slack Alert Channel`,
+    description: "Slack notification channel for monitoring alerts",
     labels: {
-      url: slack_alerting_webhook,
+      auth_token: slack_app_token,
+      channel_name: env("SLACK_ALERTING_CHANNEL_NAME"),
     },
   }
 );
@@ -69,40 +77,44 @@ const slackNotificationChannel = new gcp.monitoring.NotificationChannel(
 new gcp.monitoring.AlertPolicy(
   `${env("PROJECT_SERVICE_NAME")}-uptime-alert-policy`,
   {
-    displayName: `${env("PROJECT_SERVICE_NAME")} Uptime Alerting Policy`,
-    combiner: "OR",
+    project: env("PROJECT_ID"),
+    displayName: `${env(
+      "PROJECT_SERVICE_NAME"
+    )} Healthcheck Route Uptime Alerting Policy`,
     conditions: [
       {
+        displayName: "Uptime check monitoring",
         conditionThreshold: {
+          filter: pulumi.interpolate`resource.type = "uptime_url" AND metric.labels.check_id = "${healthcheckRouteUptimeCheck.id}" AND metric.type = "monitoring.googleapis.com/uptime_check/check_passed"`,
+          // Each region in our uptime monitor produces separate metric data (as a time series). Aggregation is required before comparison
           aggregations: [
             {
-              alignmentPeriod: "900s",
-              crossSeriesReducer: "REDUCE_COUNT_FALSE",
-              groupByFields: [
-                "resource.label.project_id",
-                "resource.label.service_name",
-                "resource.label.revision_name",
-                "resource.label.location",
-                "resource.label.configuration_name",
-              ],
+              // How the time series are aligned before cross-series reduction is applied. ALIGN_NEXT_OLDER takes the nearest previous value in the series to
+              // the current moment, which
               perSeriesAligner: "ALIGN_NEXT_OLDER",
+              // Only aggregate as often as the uptime check may have new data
+              alignmentPeriod: pulumi.interpolate`${uptime_check_period}s`,
+              // Reduce to a count of check_passed === false cases in the aligned period
+              crossSeriesReducer: "REDUCE_COUNT_FALSE",
             },
           ],
+          // Using > 2 as our condition against our reduced count, as each failing uptime check region will contribute one to the reduced count,
+          // and the minimum number of regions is three. Requiring at least two to be reporting outages, even if there's only three total, is a
+          // reasonable trade off between false-positives and false-negatives (false-negatives should only be temporary, we'd catch a true sustained
+          // outage within a few uptime check periods)
           comparison: "COMPARISON_GT",
-          filter: `metric.labels.check_id = "${healthcheckRouteUptimeCheck.id}"`,
-          thresholdValue: 0.5,
+          thresholdValue: 2,
+          // Only trigger alert if two subsequent aggregations meet the condition
           trigger: {
-            count: 1,
+            count: 2,
           },
-          duration: "120s",
+          // Amount of time the trigger has to be true for an alert to send; required but effectively redundant to our time based aggregation and
+          // comparison approach. Using 0s to keep this from complicating the logic further
+          duration: `0s`,
         },
-        displayName: "Cloud Run Revision - Check passed",
       },
     ],
+    combiner: "OR",
     notificationChannels: [slackNotificationChannel.id],
-    userLabels: {
-      // payload for the slack webhook, see slack's webhook documentation for format
-      payload: `{"text": "Uptime check failing!"}`,
-    },
   }
 );

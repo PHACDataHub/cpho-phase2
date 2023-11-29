1816437e0c0e0074aa95961457c4eb63b7dc17b8
Kaitlyn Dompaul <kaitlyndompaul@icloud.com>
2023-11-29 14:52:35 -0500

    added form validation for benchmarking

--- server/cpho/views/indicator_metadata.py
+++ server/cpho/views/indicator_metadata.py
@@ -62,9 +62,7 @@ class BenchmarkingForm(ModelForm):
     value = forms.FloatField(
         required=False,
         widget=forms.NumberInput(
-            attrs={
-                "class": "form-control",
-            }
+            attrs={"class": "form-control", "placeholder": tdt("Value")}
         ),
     )
     year = forms.IntegerField(
@@ -108,6 +106,54 @@ class BenchmarkingForm(ModelForm):
             }
         ),
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
+
+    def clean_value(self):
+        value = self.cleaned_data["value"]
+        if value and value < 0:
+            print("invalid")
+            self.add_error("value", tdt("Value cannot be negative"))
+        return value
+
+    def clean_year(self):
+        year = self.cleaned_data["year"]
+
+        if year is None or year == "":
+            return None
+
+        if year:
+            try:
+                if not (int(year) >= 2000 and int(year) <= 2050):
+                    self.add_error(
+                        "year",
+                        tdt("Year must be between the years 2000 and 2050"),
+                    )
+            except ValueError:
+                self.add_error(
+                    "year",
+                    tdt("Yearmust be a valid number"),
+                )
+
+        return year
+
+    def clean_standard_deviation(self):
+        standard_deviation = self.cleaned_data["standard_deviation"]
+
+        if standard_deviation is not None:
+            if standard_deviation < -3000 or standard_deviation > 1000:
+                self.add_error(
+                    "standard_deviation",
+                    tdt("Standard deviation must be a valid number"),
+                )
+        else:
+            self.add_error(
+                "standard_deviation", tdt("Standard deviation cannot be null")
+            )
+
+        return standard_deviation
 
     def save(self, commit=True):
         if self.cleaned_data["is_deleted"]:

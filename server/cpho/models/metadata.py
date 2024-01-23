37604e6b19b94298cf9007198c07c65bf6a9a385
AlexCLeduc <ledualex@gmail.com>
2024-01-23 12:18:42 -0500

    Add data-model for indicator-directories

--- server/cpho/models/metadata.py
+++ server/cpho/models/metadata.py
@@ -39,3 +39,72 @@ class PhacOrgRole(models.Model):
         "cpho.PHACOrg", on_delete=models.CASCADE, related_name="phac_org_roles"
     )
     is_phac_org_lead = models.BooleanField(default=False)
+
+
+@add_to_admin
+class IndicatorDirectory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    users = fields.ManyToManyField(
+        "cpho.User",
+        through="cpho.IndicatorDirectoryUserAccess",
+        related_name="indicator_directories",
+    )
+
+    indicators = fields.ManyToManyField(
+        "cpho.Indicator",
+        through="cpho.IndicatorDirectoryLink",
+        related_name="indicator_directories",
+    )
+
+    def __str__(self):
+        return self.name
+
+
+@add_to_admin
+class IndicatorDirectoryLink(models.Model):
+    """
+    through model between an indicator and an indicator directory.
+    """
+
+    indicator = fields.ForeignKey(
+        "cpho.Indicator",
+        on_delete=models.CASCADE,
+        related_name="indicator_directory_links",
+    )
+    directory = fields.ForeignKey(
+        "cpho.IndicatorDirectory",
+        on_delete=models.CASCADE,
+        related_name="indicator_directory_links",
+    )
+
+    class Meta:
+        unique_together = ("indicator", "directory")
+
+    def __str__(self):
+        return f"{self.directory} - {self.indicator}"
+
+
+@add_to_admin
+class IndicatorDirectoryUserAccess(models.Model):
+    """
+    through model between an indicator directory and a user
+    """
+
+    user = fields.ForeignKey(
+        "cpho.User",
+        on_delete=models.CASCADE,
+        related_name="indicator_directory_user_accesses",
+    )
+    directory = fields.ForeignKey(
+        "cpho.IndicatorDirectory",
+        on_delete=models.CASCADE,
+        related_name="indicator_directory_user_accesses",
+    )
+
+    class Meta:
+        unique_together = ("user", "directory")
+
+    def __str__(self):
+        return f"{self.user} - {self.directory}"

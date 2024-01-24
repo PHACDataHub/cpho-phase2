27fbe403d40270464abe83cb7231bfb584e66ba3
msarar <galaxyraad@gmail.com>
2024-01-24 17:24:31 -0500

    forgot to save remainder of test file before pushing

--- server/tests/test_manual_trend_analysis_input.py
+++ server/tests/test_manual_trend_analysis_input.py
@@ -61,7 +61,7 @@ def test_trend_analysis(vanilla_user_client):
         "trend_analysis-0-id": data2020.id,
         "trend_analysis-0-year": "2020",
         "trend_analysis-0-year_range": "2019-2020",
-        "trend_analysis-0-data_point": 9.0,
+        "trend_analysis-0-data_point": 9.0,  # change 9.9 to 9.0
         "trend_analysis-0-trend_segment": "2019-2020",
         "trend_analysis-0-trend": TrendAnalysis.TREND_CHOICES[1][0],
         "trend_analysis-1-id": data2021.id,
@@ -70,5 +70,21 @@ def test_trend_analysis(vanilla_user_client):
         "trend_analysis-1-data_point": 10.9,
         "trend_analysis-1-trend_segment": "2020-2021",
         "trend_analysis-1-trend": TrendAnalysis.TREND_CHOICES[2][0],
-        "trend_analysis-1-is_deleted": "on",  # delete 2022
+        "trend_analysis-1-is_deleted": "on",  # delete 2021
     }
+
+    with patch_rules(can_edit_trend_analysis=True):
+        response = vanilla_user_client.post(url, data=data)
+        assert response.status_code == 302
+
+    created_data = TrendAnalysis.active_objects.filter(indicator=ind)
+
+    assert created_data.count() == 1
+    data2020 = created_data.get(year="2020")
+    assert data2020.data_point == 9.0
+
+    all_data = TrendAnalysis.objects.filter(indicator=ind)
+    assert all_data.count() == 2
+    data2020 = all_data.get(year="2021")
+    assert data2020.is_deleted == True
+    assert data2020.deletion_time is not None

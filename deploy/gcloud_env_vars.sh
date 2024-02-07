d98f886e70e57a3a1d875c07ac513a10a5856864
Vedant Thapa <43611693+vedantthapa@users.noreply.github.com>
2024-02-07 18:34:32 -0400

    Configure IaD manifests (#193)
    * feat: add gcloud commands to create resources necessary for k8s cluster
    
    * feat: add kcc configs
    
    * feat: update project id
    
    * feat: update name, service account and branch name for flux
    
    * feat: update cluster creation cmd and add role binding for asm
    
    * feat: update sa name and add project annotation
    
    * feat: add service usage api
    
    * feat: add infrastructure manifests
    
    * feat: remove wi-test pod def
    
    * Update cloudbuild variables for test run in new project infrastructure
    
    * feat: add / update buckets and iam-mappings for buckets
    
    * feat: add descriptive names instead of general 'hopic' name
    
    * fix: update managed zone ref
    
    * feat: move labels to kustomize
    
    * Re-sync legacy gcloud_env_var.sh values to new IaD manifests. To be deprecated for something more integrated soon
    
    * fix: enable cloudbuild trigger
    
    * Update test coverage bucket name in the cloud build env vars
    
    ---------
    
    Co-authored-by: Stephen-ONeil <stephenlevioneil@gmail.com>

--- deploy/gcloud_env_vars.sh
+++ deploy/gcloud_env_vars.sh
@@ -10,10 +10,10 @@
 # Project dependent #
 #####################
 
-export PROJECT_ID=phx-01h4rr1468rj3v5k60b1vserd3
+export PROJECT_ID=pht-01hp04dtnkf
 export PROJECT_REGION=northamerica-northeast1
 
-export PROJECT_SERVICE_NAME=cpho-phase2
+export PROJECT_SERVICE_NAME=hopic
 export PROJECT_ALPHA_SUB_DOMAIN=hopic-sdpac
 
 export BUILD_GITHUB_REPO_NAME=cpho-phase2
@@ -21,7 +21,7 @@ export BUILD_GITHUB_REPO_OWNER=PHACDataHub
 
 export GITHUB_MAIN_BRANCH_NAME=main
 
-export TEST_COVERAGE_BUCKET_NAME=hopic-test-coverage-reports
+export TEST_COVERAGE_BUCKET_NAME=hopic-test-coverage-reports-01hp04dtnkf
 export TEST_COVERAGE_THRESHOLD=77.5 # set just below current coverage, consider increasing in time
 export TEST_DELTA_THRESHOLD=-2  # % value, threshold for current commit test coverage minus last commit on main's coverage
 
@@ -44,16 +44,16 @@ export DNS_PHAC_ALPHA_DOMAIN="phac-aspc.alpha.canada.ca"
 export DNS_PHAC_ALPHA_NAME="phac-aspc-alpha-canada-ca"
 export DNS_PHAC_ALPHA_NS_NAME="alpha-dns"
 # project DNS vars
-export DNS_MANAGED_ZONE_NAME="${PROJECT_SERVICE_NAME}-managed-dns-zone"
+export DNS_MANAGED_ZONE_NAME="${PROJECT_SERVICE_NAME}-managed-zone"
 export DNS_DOMAIN="${PROJECT_ALPHA_SUB_DOMAIN}.${DNS_PHAC_ALPHA_DOMAIN}"
 export DNS_DNS_NAME="${DNS_DOMAIN}."
 export DNS_NS_NAME="${PROJECT_ALPHA_SUB_DOMAIN}-${DNS_PHAC_ALPHA_NAME}-ns"
 
 # ----- ARTIFACT REGISTRY -----
-export ARTIFACT_REGISTRY_REPO="${PROJECT_SERVICE_NAME}-artifact-registry-for-cloud-run"
+export ARTIFACT_REGISTRY_REPO="${PROJECT_SERVICE_NAME}-k8s-images"
 
 # ----- CLOUD BUILD ----
-export BUILD_CLOUD_BUILD_TRIGGER_NAME="${PROJECT_SERVICE_NAME}-github-main-branch-trigger"
+export BUILD_CLOUD_BUILD_TRIGGER_NAME="${PROJECT_SERVICE_NAME}-cloudbuild-trigger"
 export BUILD_CLOUD_BUILD_CONFIG_PATH=cloudbuild.yaml
 export BUILD_CLOUD_RUN_IMAGE_NAME="${PROJECT_REGION}-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/${BUILD_GITHUB_REPO_NAME}"
 

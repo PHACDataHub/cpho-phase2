96d40f9bdf46b4decb588c8cfdb0e92a97e4af72
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-02-07 15:21:00 -0500

    Update cloudbuild variables for test run in new project infrastructure

--- deploy/gcloud_env_vars.sh
+++ deploy/gcloud_env_vars.sh
@@ -10,10 +10,10 @@
 # Project dependent #
 #####################
 
-export PROJECT_ID=phx-01h4rr1468rj3v5k60b1vserd3
+export PROJECT_ID=pht-01hp04dtnkf
 export PROJECT_REGION=northamerica-northeast1
 
-export PROJECT_SERVICE_NAME=cpho-phase2
+export PROJECT_SERVICE_NAME=hopic
 export PROJECT_ALPHA_SUB_DOMAIN=hopic-sdpac
 
 export BUILD_GITHUB_REPO_NAME=cpho-phase2
@@ -44,16 +44,16 @@ export DNS_PHAC_ALPHA_DOMAIN="phac-aspc.alpha.canada.ca"
 export DNS_PHAC_ALPHA_NAME="phac-aspc-alpha-canada-ca"
 export DNS_PHAC_ALPHA_NS_NAME="alpha-dns"
 # project DNS vars
-export DNS_MANAGED_ZONE_NAME="${PROJECT_SERVICE_NAME}-managed-dns-zone"
+export DNS_MANAGED_ZONE_NAME="${PROJECT_SERVICE_NAME}"
 export DNS_DOMAIN="${PROJECT_ALPHA_SUB_DOMAIN}.${DNS_PHAC_ALPHA_DOMAIN}"
 export DNS_DNS_NAME="${DNS_DOMAIN}."
 export DNS_NS_NAME="${PROJECT_ALPHA_SUB_DOMAIN}-${DNS_PHAC_ALPHA_NAME}-ns"
 
 # ----- ARTIFACT REGISTRY -----
-export ARTIFACT_REGISTRY_REPO="${PROJECT_SERVICE_NAME}-artifact-registry-for-cloud-run"
+export ARTIFACT_REGISTRY_REPO="${PROJECT_SERVICE_NAME}"
 
 # ----- CLOUD BUILD ----
-export BUILD_CLOUD_BUILD_TRIGGER_NAME="${PROJECT_SERVICE_NAME}-github-main-branch-trigger"
+export BUILD_CLOUD_BUILD_TRIGGER_NAME="${PROJECT_SERVICE_NAME}-main"
 export BUILD_CLOUD_BUILD_CONFIG_PATH=cloudbuild.yaml
 export BUILD_CLOUD_RUN_IMAGE_NAME="${PROJECT_REGION}-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/${BUILD_GITHUB_REPO_NAME}"
 

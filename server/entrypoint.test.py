9a58282897375468aa6343386d5b0304bd8eb666
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-03-19 14:52:19 -0400

    Use "python -m" to run python modules as scripts in prod docker file entrypoints, the distroless image isn't finding them otherwise

--- server/entrypoint.test.py
+++ server/entrypoint.test.py
@@ -2,14 +2,18 @@ import subprocess
 import sys
 
 sys.stdout.write("\nRunning tests...\n")
-subprocess.run(["coverage", "run", "-m", "pytest"], check=True)
+subprocess.run(["python", "-m", "coverage", "run", "-m", "pytest"], check=True)
 
 sys.stdout.write("\nCoverage report:\n")
-subprocess.run(["coverage", "report", "--show-missing"], check=True)
+subprocess.run(
+    ["python", "-m", "coverage", "report", "--show-missing"], check=True
+)
 
 sys.stdout.write("\nWritting coverage.json...\n")
 subprocess.run(
     [
+        "python",
+        "-m",
         "coverage",
         "json",
         "-o",

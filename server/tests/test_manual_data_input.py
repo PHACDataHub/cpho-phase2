a8575d63a15bda3c6c07af442b99110dbdfcef0e
msarar <galaxyraad@gmail.com>
2024-05-06 16:23:37 -0400

    added test for readonly indicator data

--- server/tests/test_manual_data_input.py
+++ server/tests/test_manual_data_input.py
@@ -4,6 +4,7 @@ from phac_aspc.rules import patch_rules
 
 from cpho.model_factories import IndicatorDatumFactory, IndicatorFactory
 from cpho.models import DimensionType, DimensionValue, IndicatorDatum, Period
+from cpho.views import IndicatorDatumForm, ReadOnlyIndicatorDatumForm
 
 
 def test_predefined_create_from_scratch(vanilla_user_client):
@@ -430,3 +431,51 @@ def test_non_changes_dont_create_versions(vanilla_user_client):
 
     assert male_record.versions.count() == 1
     assert record0_25.versions.count() == 1
+
+
+def test_readonly_indicator_data(vanilla_user_client):
+    period = Period.objects.first()
+    ind = IndicatorFactory()
+    age_cat = DimensionType.objects.get(code="age")
+
+    record0_25 = ind.data.create(
+        period=period,
+        dimension_type=age_cat,
+        literal_dimension_val="0-25",
+        value=5,
+    )
+    sex_cat = DimensionType.objects.get(code="sex")
+
+    male_dimension_value = sex_cat.possible_values.get(value="m")
+
+    male_record = ind.data.create(
+        period=period,
+        dimension_type=sex_cat,
+        dimension_value=male_dimension_value,
+        value=6,
+    )
+
+    url = reverse("manage_indicator_data_all", args=[ind.id, period.id])
+    with patch_rules(
+        can_edit_indicator_data=False, can_view_indicator_data=True
+    ):
+        response = vanilla_user_client.get(url)
+        assert response.status_code == 200
+        formset = response.context["age_group_formset"]
+        for form in formset:
+            assert isinstance(form, ReadOnlyIndicatorDatumForm)
+        formset = response.context["predefined_values_formset"]
+        for form in formset:
+            assert isinstance(form, ReadOnlyIndicatorDatumForm)
+
+    with patch_rules(
+        can_edit_indicator_data=True, can_view_indicator_data=True
+    ):
+        response = vanilla_user_client.get(url)
+        assert response.status_code == 200
+        formset = response.context["age_group_formset"]
+        for form in formset:
+            assert isinstance(form, IndicatorDatumForm)
+        formset = response.context["predefined_values_formset"]
+        for form in formset:
+            assert isinstance(form, IndicatorDatumForm)

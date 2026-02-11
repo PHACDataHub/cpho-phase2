56f2a253df7d2444171b440d53bc16e119639216
Keenan <keenandhanani@gmail.com>
2026-02-11 10:07:08 -0500

    remove telementary gunicorn

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -3,8 +3,6 @@ import sys
 
 import structlog
 
-from server.open_telemetry_util import instrument_app_for_open_telemetry
-
 # See https://cloud.google.com/run/docs/tips/python#optimize_gunicorn
 
 PORT = os.getenv("PORT", "8080")
@@ -26,7 +24,7 @@ def post_fork(server, worker):
     # If NOT using BatchSpanProcessor (likely a bad idea, it's much more performant at run time) you can move instrumentation to wsgi.py and enable preload_app
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
-    worker.flush_telemetry_callback = instrument_app_for_open_telemetry()
+    # worker.flush_telemetry_callback = instrument_app_for_open_telemetry()
 
 
 def worker_exit(server, worker):
